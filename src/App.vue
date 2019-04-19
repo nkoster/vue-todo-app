5c5c3892e7295cdc3dbdfbcb3f2e39b5b9b2fbdc
Niels Koster <n.koster@portavita.eu>
2019-04-20 01:44:58 +0200

    Add vue-router, move Header

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,16 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
 import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-import axios from 'axios';
-
 export default {
   name: 'app',
   components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: []
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-      axios.post('https://jsonplaceholder.typicode.com/todos', {
-        title,
-        completed
-      }).then(res => this.todos = [...this.todos, res.data])
-      .catch(err => console.log(err))
-    }
-  },
-  created() {
-  axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-    .then(res => this.todos = res.data)
-    .catch(err => console.log(err))
+    Header
   }
 }
 </script>
